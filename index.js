a6f74716cc88b5a0c3a79a7946ab87e70bbeee5f
Tahsin Nurun <nuruntahsin@gmail.com>
2021-08-14 00:35:22 +0600

    003

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ proImg.forEach((item, index) => {
 // sorting code
 
 function sortBy() {
+
+    
     var select = document.getElementById("sort-by");
     console.log(select);
     var selectedValue = select.value;
@@ -57,16 +59,77 @@ function sortBy() {
 
 window.addEventListener('DOMContentLoaded', (event) => {
     var allProducts = document.querySelectorAll(".male,.female");
-    for(i=0; i < 6; i++){
+    for (i = 0; i < 18; i++) {
         allProducts[i].classList.remove("d-none");
     }
 });
 
 // load product based on dropdown
 
-function loadProduct(value){
+function loadProduct(value) {
     var allProducts = document.querySelectorAll(".male,.female");
-    for(i=0; i < value; i++){
+    for (i = 0; i < value; i++) {
         allProducts[i].classList.remove("d-none");
     }
+}
+
+// New/sale filter code
+
+
+
+var itemsToFilter = document.querySelectorAll(".male,.female");
+// console.log(itemsToFilter);
+var checkBoxes = document.querySelectorAll(".form-check-input");
+// console.log(checkBoxes);
+for (var i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].addEventListener("click", filterItems, false);
+}
+
+function filterItems(e) {
+    var clickedItem = e.target;
+    if (clickedItem.value == true) {
+        hideOrShowItems(clickedItem.value, "hideItem", "showItem")
+    } else if (clickedItem.value == false) {
+        hideOrShowItems(clickedItem.value, "showItem", "hideItem");
+    } else { }
+}
+
+function hideOrShowItems(itemType, classToRemove, classToAdd){
+    for(var i = 0; i < itemsToFilter.length; i++){
+        var currentItem = itemsToFilter[i];
+        if(currentItem.getAttribute("data-type")==itemType){
+            removeClass(currentItem, classToRemove);
+            addClass(currentItem, classToAdd);
+        }
+    }
+}
+
+function addClass(element,classToAdd){
+    var currentClassValue = element.className;
+
+    if(currentClassValue.indexOf(classToAdd) == -1){
+        if((currentClassValue ==null)||(currentClassValue=="")){
+            element.className = classToAdd;
+        }else{
+            element.className += " "+classToAdd;
+        }
+    }
+}
+
+function removeClass(element, classToRemove){
+    var currentClassValue = element.className;
+    if(currentClassValue == classToRemove){
+        element.className = " ";
+        return;
+    }
+
+    var classValues = currentClassValue.split(" ");
+    var filterList =[];
+
+    for(var i=0; i<classValues.length; i++){
+        if(classToRemove != classValues[i]){
+            filterList.push(classValue[i]);
+        }
+    }
+    element.className = filterList.join(" ");
 }
